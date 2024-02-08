f54f4ab3d4d5b2262c7ecd861fad37698837f6b2
DiegoTalledoS18 <85322967+DiegoTalledoS18@users.noreply.github.com>
2024-02-07 20:59:32 -0500

    adding message

---
+++ server/index.js
@@ -0,0 +1,32 @@
+import express from 'express'
+import logger from 'morgan'
+
+import {Server} from 'socket.io'
+import { createServer } from 'node:http'
+
+const port=process.env.PORT ?? 3000
+const app =express()
+const server =createServer(app)
+const io = new Server(server)
+
+io.on('connection',(socket)=>{
+    console.log('a user has connected')
+
+    socket.on('disconnect',()=>{
+        console.log('a user has disconnected')
+    })
+
+    socket.on('chat message', async (msg) => {
+        io.emit('chat message', msg)
+    })
+})
+
+app.use(logger('dev'))
+app.get('/',(req,res)=>{
+    res.sendFile(process.cwd()+'/client/pages/index.html')
+    console.log()
+})
+
+server.listen(port,()=>{
+    console.log(`server running on port http://localhost:${port}`)
+})
